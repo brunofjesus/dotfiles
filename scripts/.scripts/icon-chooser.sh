17a70fbddcfdb46ba9e95b14039d6e8b37448b30
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-01-27 00:28:57 +0000

    make icon and theme chooser gui compatible

--- scripts/.scripts/icon-chooser.sh
+++ scripts/.scripts/icon-chooser.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+RUNNER="wofi -i --dmenu"
+if tty -s; then
+    RUNNER="fzf"
+fi
+
 # List icon themes
 ICON_THEMES=$(find /usr/share/icons/ ~/.icons/ -maxdepth 1 -type d | awk -F/ '{print $NF}' | sort | uniq)
 
 # Use `fzf` for interactive selection (or `wofi`/`rofi` if desired)
-SELECTED_ICON_THEME=$(echo "$ICON_THEMES" | fzf --prompt="Select an icon theme: ")
+SELECTED_ICON_THEME=$(echo "$ICON_THEMES" | $RUNNER --prompt="Select an icon theme: ")
 
 if [ -n "$SELECTED_ICON_THEME" ]; then
   echo "Applying icon theme: $SELECTED_ICON_THEME"
--- scripts/.scripts/theme-chooser.sh
+++ scripts/.scripts/theme-chooser.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+RUNNER="wofi -i --dmenu"
+if tty -s; then
+    RUNNER="fzf"
+fi
+
 # List themes
 THEMES=$(ls /usr/share/themes/ ~/.themes/ 2>/dev/null | sort | uniq)
 
 # Use `fzf` for interactive selection or `wofi`/`rofi` if you prefer
-SELECTED_THEME=$(echo "$THEMES" | fzf --prompt="Select a GTK theme: ")
+SELECTED_THEME=$(echo "$THEMES" | $RUNNER --prompt="Select a GTK theme: ")
 
 if [ -n "$SELECTED_THEME" ]; then
   echo "Applying theme: $SELECTED_THEME"
