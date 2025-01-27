a0ac8b29956c8e4ea383a5b947fcaa694955b758
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-01-27 00:24:19 +0000

    Make reddit-video-downloader support gui

--- scripts/.scripts/reddit-video-downloader.sh
+++ scripts/.scripts/reddit-video-downloader.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
-URL=$1
-TITLE=$(echo $URL| cut -d'/' -f 8)
-
-if [ $# = 2 ]
-then
-    if [ -d $2 ]
-    then
-        if [ ${2: -1} = "/" ]
-        then
-            OUTPUT="${2}${TITLE}"
-        else
-            OUTPUT="${2}/${TITLE}"
-        fi
-    else
-        OUTPUT="${2%.*}"
+# Get URL from command line or zenity
+if [ $# -ge 1 ]; then
+    URL=$1
+else
+    URL=$(zenity --entry \
+        --entry-text="$(wl-paste)" \
+        --title="Reddit Video Downloader" \
+        --text="Enter the Reddit video URL:" \
+        --width=800)
+    
+    # Check if user clicked cancel
+    if [ $? -ne 0 ]; then
+        echo "Download cancelled"
+        exit 1
     fi
-elif [ $# = 1 ]
-then
-    OUTPUT=$TITLE
+    
+    # Check if URL is empty
+    if [ -z "$URL" ]; then
+        zenity --error \
+            --text="URL cannot be empty" \
+            --width=200
+        exit 1
+    fi
 fi
 
-ffmpeg -i $(wget -qO- "https://api.reddit.com/api/info/?id=t3_$(echo $URL| cut -d'/' -f 7)" | jq -r '.data.children[0].data.secure_media.reddit_video.dash_url') -c copy "${OUTPUT}.mp4"
+TITLE=$(echo $URL| cut -d'/' -f 8)
+
+ffmpeg -i $(wget -qO- "https://api.reddit.com/api/info/?id=t3_$(echo $URL| cut -d'/' -f 7)" | jq -r '.data.children[0].data.secure_media.reddit_video.dash_url') -c copy "$HOME/Videos/${TITLE}.mp4"
