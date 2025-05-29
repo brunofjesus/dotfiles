42005249aa1d9eef79b7c4d06ed6a311a7557f78
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-05-29 18:51:15 +0100

    open camera grid

--- scripts/.scripts/cctv.sh
+++ scripts/.scripts/cctv.sh
@@ -7,23 +7,22 @@ else
   source ~/.env
 fi
 
-OPTIONS="🛋️ Sala\n🧑‍🍳 Cozinha\n🚘 Garagem\n🚪 Entrada"
+declare -A camera_map
+camera_map["sala"]="rtsp://admin:${CCTV_PASSWORD}@172.30.1.90/Streaming/Channels/101";
+camera_map["cozinha"]="rtsp://admin:${CCTV_PASSWORD}@172.30.1.91/Streaming/Channels/101";
+camera_map["garagem"]="rtsp://admin:${CCTV_PASSWORD}@172.30.1.92/Streaming/Channels/101";
+camera_map["entrada"]="rtsp://admin:${CCTV_PASSWORD}@172.30.1.93/Streaming/Channels/101";
+
+OPTIONS="🛋️ Sala\n🧑‍🍳 Cozinha\n🚘 Garagem\n🚪 Entrada\n🔳 Grid"
 
 CHOICE=$(echo -e "$OPTIONS" | $RUNNER --prompt "Camera: " | awk '{print tolower($2)}' )
 
 
 case "$CHOICE" in
-    "sala")
-        mpv rtsp://admin:${CCTV_PASSWORD}@172.30.1.90/Streaming/Channels/101
-        ;;
-    "cozinha")
-        mpv rtsp://admin:${CCTV_PASSWORD}@172.30.1.91/Streaming/Channels/101
-        ;;
-
-    "garagem")
-        mpv rtsp://admin:${CCTV_PASSWORD}@172.30.1.92/Streaming/Channels/101
-        ;;
-    "entrada")
-        mpv rtsp://admin:${CCTV_PASSWORD}@172.30.1.93/Streaming/Channels/101
-        ;;
+    "sala"|"cozinha"|"garagem"|"entrada")
+      mpv ${camera_map[$CHOICE]}
+      ;;
+    "grid")
+      swaymsg "exec mpv ${camera_map[sala]}; exec mpv ${camera_map[cozinha]}" && sleep 3 && swaymsg "splitt" && swaymsg "exec mpv ${camera_map[garagem]}" && sleep 3 && swaymsg "focus left; focus up; splitt" && swaymsg "exec mpv ${camera_map[entrada]}"
+      ;;
 esac
