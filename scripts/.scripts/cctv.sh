36e9431204153ddbbda3f92e3d104138a8d3479e
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-06-03 23:39:35 +0100

    improve cctv grid

--- scripts/.scripts/cctv.sh
+++ scripts/.scripts/cctv.sh
@@ -23,6 +23,49 @@ case "$CHOICE" in
       mpv ${camera_map[$CHOICE]}
       ;;
     "grid")
-      swaymsg "exec mpv ${camera_map[sala]}; exec mpv ${camera_map[cozinha]}" && sleep 3 && swaymsg "splitt" && swaymsg "exec mpv ${camera_map[garagem]}" && sleep 3 && swaymsg "focus left; focus up; splitt" && swaymsg "exec mpv ${camera_map[entrada]}"
+      TEMPFILE_SALA=$(mktemp /tmp/cctv.sala.XXXXXX)
+      TEMPFILE_COZINHA=$(mktemp /tmp/cctv.cozinha.XXXXXX)
+      TEMPFILE_GARAGEM=$(mktemp /tmp/cctv.garagem.XXXXXX)
+      TEMPFILE_ENTRADA=$(mktemp /tmp/cctv.entrada.XXXXXX)
+
+      # top left
+      swaymsg "exec mpv --force-window --idle=yes --input-ipc-server=${TEMPFILE_SALA}"
+      sleep 0.5
+
+      # top right
+      swaymsg split v
+      swaymsg "exec mpv --force-window --idle=yes --input-ipc-server=${TEMPFILE_ENTRADA}"
+      sleep 0.5
+
+      # bottom left
+      swaymsg focus left
+      swaymsg split h
+      swaymsg "exec mpv --force-window --idle=yes --input-ipc-server=${TEMPFILE_GARAGEM}"
+      sleep 0.5
+
+      # bottom right
+      swaymsg focus up
+      swaymsg focus right
+      swaymsg split h
+      swaymsg "exec mpv --force-window --idle=yes --input-ipc-server=${TEMPFILE_COZINHA}"
+      sleep 0.5
+
+      printf '{ "command": ["loadfile", "%s"] }\n' "${camera_map["sala"]}" | nc -U "$TEMPFILE_SALA" &
+      PID_SALA=$!
+      printf '{ "command": ["loadfile", "%s"] }\n' "${camera_map["cozinha"]}" | nc -U "$TEMPFILE_COZINHA" &
+      PID_COZINHA=$!
+      printf '{ "command": ["loadfile", "%s"] }\n' "${camera_map["garagem"]}" | nc -U "$TEMPFILE_GARAGEM" &
+      PID_GARAGEM=$!
+      printf '{ "command": ["loadfile", "%s"] }\n' "${camera_map["entrada"]}" | nc -U "$TEMPFILE_ENTRADA" &
+      PID_ENTRADA=$!
+
+      # Wait for all mpv processes to exit
+      wait $PID_SALA
+      wait $PID_COZINHA
+      wait $PID_GARAGEM
+      wait $PID_ENTRADA
+      
+      # Now remove the temp files
+      rm -f "$TEMPFILE_SALA" "$TEMPFILE_COZINHA" "$TEMPFILE_GARAGEM" "$TEMPFILE_ENTRADA"
       ;;
 esac
