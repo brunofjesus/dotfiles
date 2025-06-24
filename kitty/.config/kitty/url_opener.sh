7ba49e0263a93987cfb27313c5331a2dc309f73b
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-06-24 22:30:38 +0100

    disable open with mpv until I craft a better solution

--- kitty/.config/kitty/url_opener.sh
+++ kitty/.config/kitty/url_opener.sh
@@ -12,12 +12,13 @@ if [ -z "$URL" ]; then
     exit 1
 fi
 
-# Check if the URL's domain is youtube.com
-if echo "$URL" | grep -E "https?://(www\.)?youtube\.com(/.*)?"; then
-    echo "Opening YouTube URL with mpv..."
-    mpv "$URL"
-else
-    echo "Opening URL with Brave browser..."
-    brave "$URL"
-fi
+## Check if the URL's domain is youtube.com
+#if echo "$URL" | grep -E "https?://(www\.)?youtube\.com(/.*)?"; then
+#    echo "Opening YouTube URL with mpv..."
+#    mpv "$URL"
+#else
+#    echo "Opening URL with Brave browser..."
+#    brave "$URL"
+#fi
 
+brave "$URL"
