ea64fc696d1761803a9fb918ccc036f61b697e19
Bruno Jesus <bruno.fl.jesus@gmail.com>
2026-04-21 17:08:03 +0100

    fix waybar script

--- waybar/.config/waybar/waybar.sh
+++ waybar/.config/waybar/waybar.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-while [ true ]; do
+# Abort if another instance of this script is running
+script_name=$(basename "$0")
+if [ "$(pgrep -c -x "$script_name")" -gt 1 ]; then
+  exit 1
+fi
+
+# Abort if waybar is already running
+if pgrep -x waybar > /dev/null; then
+  exit 1
+fi
+
+while true; do
   waybar
 done
